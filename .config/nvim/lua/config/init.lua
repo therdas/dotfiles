d7e18d794937c01f30cd58653d1568d90325ad16
therdas <git@therdas.dev>
2025-02-21 18:53:36 +0530

    Created initial config files, started nvim setup

---
+++ .config/nvim/lua/config/init.lua
@@ -0,0 +1,3 @@
+require("config.remap")
+require("config.lazy")
+print("Hello from config")
---
+++ .config/nvim/lua/plugins/cloak.lua
@@ -0,0 +1,38 @@
+local options = {
+	enabled = true,
+	cloak_character = "*",
+	cloak_length = "8",
+	try_all_patterns = true,
+	cloak_telescope = true,
+	cloak_on_leave = false,
+	patterns = {
+		{
+			file_pattern = '.env*',
+			cloak_pattern = '=.+',
+			replace = nil
+		},
+		{
+			file_pattern = '.ini',
+			cloak_pattern = {
+				'key=.*',
+				'key:.*',
+			}
+		},
+		{
+			file_pattern = '.ini',
+			cloak_pattern = {
+				'key=.*',
+				'key:.*',
+			}
+		}
+	}
+}
+
+return {
+	"laytan/cloak.nvim",
+	event = "VeryLazy",
+	config = function()
+		print "loading cloak"
+		require("cloak").setup(options)
+	end,
+}
